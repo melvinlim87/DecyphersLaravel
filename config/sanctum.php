398c0ae2e706c3e8cd1a54c7b2f27029cda95036
James <bagcaljames@gmail.com>
2025-04-25 17:39:14 +0800

    csrf fix

--- config/sanctum.php
+++ config/sanctum.php
@@ -19,6 +19,7 @@ return [
         '%s%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:3000,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
+        'http://decyphers.com',
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
 
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api', // Replace with your API base URL if needed
+  baseURL: 'http://localhost:8000/api', // Replace with your API base URL if needed
   withCredentials: true // Include cookies in requests
 });
 
